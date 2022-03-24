3f212a328468b811485a645092253ec2136af142
acnow <acvarela77@gmail.com>
2022-03-24 13:33:09 -0700

    added serializer1 to when user crreate a post only

--- api/serializers/blog.py
+++ api/serializers/blog.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from ..models.blog import Blog
+from .user import UserSerializer
 
 class BlogSerializer(serializers.ModelSerializer):
+    author = UserSerializer(many=False, read_only=True)
     class Meta:
         model = Blog
-        fields = ('id', 'title', 'content', 'created_at','updated_at', 'author')
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'author')
 
 class UpdateBlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = ('id', 'title', 'updated_at', 'author')
+
+class BlogSerializer1(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ('id', 'title', 'content', 'created_at', 'author')

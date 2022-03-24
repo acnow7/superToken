1953abc24af7c6ad61a1abf6f98db754fe8dcad7
acnow <acvarela77@gmail.com>
2022-03-24 15:42:47 -0700

    comments serializers created

---
+++ api/serializers/comments.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from ..models.comments import Comment
+from .user import UserSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(many=False, read_only=True)
+    # blog = BlogSerializer1()(many=False, read_only=True)
+    class Meta:
+        model = Comment
+        fields = ('id', 'content', 'created_at', 'updated_at', 'author', 'blog')
+
+class UpdatedCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'content', 'updated_at', 'author')
+
+class CommentSerializer1(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'content', 'created_at', 'author', 'blog')

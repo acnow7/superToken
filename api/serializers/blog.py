094c6ea75e2d22064c14e6a4392c637ce8aa2c2d
acnow <acvarela77@gmail.com>
2022-03-24 12:26:18 -0700

    added class for updateSerializer

--- api/serializers/blog.py
+++ api/serializers/blog.py
@@ -4,4 +4,9 @@ from ..models.blog import Blog
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = ('id', 'title', 'content', 'created_at', 'author')
+        fields = ('id', 'title', 'content', 'created_at','updated_at', 'author')
+
+class UpdateBlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ('id', 'title', 'updated_at', 'author')

f638de133b4116bfb3193121ecbf0b2f9ad153cd
acnow <acvarela77@gmail.com>
2022-03-24 11:00:57 -0700

    added url path for update password

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views.user import SignUp, SignIn, SignOut
+from .views.user import SignUp, SignIn, SignOut, ChangePasswordView
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
+    path('update-password/', ChangePasswordView.as_view(), name='update-password')
 ]

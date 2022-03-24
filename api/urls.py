02cc3f40a4abecf3f2c3c21d282dd1a9f0b78d9e
acnow <acvarela77@gmail.com>
2022-03-24 12:26:46 -0700

    added url path for get/update/delete single blog

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views.user import SignUp, SignIn, SignOut, ChangePasswordView
-from .views.blog import BlogsView
+from .views.blog import BlogsView, BlogView
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
     path('update-password/', ChangePasswordView.as_view(), name='update-password'),
-    path('blogs/', BlogsView().as_view(), name='blogs'),
+    path('blogs/', BlogsView.as_view(), name='blogs'),
+    path('blogs/<int:pk>', BlogView.as_view(), name='my-blogs'),
 ]

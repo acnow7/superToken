787a19e21760c78c7726f41622e6ac8c192f633e
acnow <acvarela77@gmail.com>
2022-03-24 15:41:20 -0700

    added url path for comments post

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.user import SignUp, SignIn, SignOut, ChangePasswordView
 from .views.blog import BlogsView, BlogView, MyBlogsView
+from .views.comments import CommentView
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('update-password/', ChangePasswordView.as_view(), name='update-password'),
     path('blogs/', BlogsView.as_view(), name='blogs'),
     path('blogs/<int:pk>', BlogView.as_view(), name='blogs'),
-    path('my-blogs/', MyBlogsView.as_view(), name='my-blogs')
+    path('my-blogs/', MyBlogsView.as_view(), name='my-blogs'),
+    path('blogs/<int:pk>/comments', CommentView.as_view(), name='comments' )
 ]

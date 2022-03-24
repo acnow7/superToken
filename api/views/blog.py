2ff20d40051520cf4f85f9581f994ccae23a9e6e
acnow <acvarela77@gmail.com>
2022-03-24 12:27:24 -0700

    added Blog class controller for CRUD functionality on user blogs

--- api/views/blog.py
+++ api/views/blog.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..serializers.blog import BlogSerializer
+from ..serializers.blog import BlogSerializer, UpdateBlogSerializer
 from ..models.blog import Blog
 from rest_framework import status
 from django.shortcuts import get_object_or_404
@@ -21,3 +21,24 @@ class BlogsView(APIView):
             return Response(blog.data, status=status.HTTP_201_CREATED)
         else:
             return Response(blog.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BlogView(APIView):
+    def get(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        data = BlogSerializer(blog).data
+        return Response(data)
+
+    def put(self, request, pk):
+        request.data['author'] = request.user.id
+        blog = get_object_or_404(Blog, pk=pk)
+        updated_blog = UpdateBlogSerializer(blog, data=request.data)
+        if updated_blog.is_valid():
+            updated_blog.save()
+            return Response(updated_blog.data)
+        else:
+            return Response(updated_blog.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

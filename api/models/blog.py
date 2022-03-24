88da17aa0937587f1714a8549fbf2481021bf51b
acnow <acvarela77@gmail.com>
2022-03-24 11:37:24 -0700

    created blog model

---
+++ api/models/blog.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=400)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    

86d9a5ae62a5f8c1734ee1c242606baca829e278
acnow <acvarela77@gmail.com>
2022-03-24 15:39:46 -0700

    added comments populated to blogs serializer

--- api/serializers/blog.py
+++ api/serializers/blog.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 from ..models.blog import Blog
 from .user import UserSerializer
+from .comments import CommentSerializer1
 
 class BlogSerializer(serializers.ModelSerializer):
     author = UserSerializer(many=False, read_only=True)
+    comments = CommentSerializer1(many=True, read_only=True)
     class Meta:
         model = Blog
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'author')
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'author', 'comments')
 
 class UpdateBlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = ('id', 'title', 'updated_at', 'author')
+        fields = ('id', 'content', 'updated_at', 'author')
 
 class BlogSerializer1(serializers.ModelSerializer):
     class Meta:

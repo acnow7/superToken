5c37a159d2aec52850e938de759100da1f3fadee
acnow <acvarela77@gmail.com>
2022-03-24 12:05:52 -0700

    added url path for get/post user blogs

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views.user import SignUp, SignIn, SignOut, ChangePasswordView
+from .views.blog import BlogsView
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
-    path('update-password/', ChangePasswordView.as_view(), name='update-password')
+    path('update-password/', ChangePasswordView.as_view(), name='update-password'),
+    path('blogs/', BlogsView().as_view(), name='blogs'),
 ]

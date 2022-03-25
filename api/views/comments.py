ab648dc993d24d3501aaf48122ca9d085776905a
acnow <acvarela77@gmail.com>
2022-03-24 17:26:55 -0700

    created updateComment controller put route

--- api/views/comments.py
+++ api/views/comments.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..serializers.comments import CommentSerializer1
+from ..serializers.comments import CommentSerializer1, UpdatedCommentSerializer
 from ..models.comments import Comment
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
 from ..models.blog import Blog
+
 class CommentView(APIView):
     # def get(self, request):
     #      # filter for mangos with our user id
@@ -24,3 +25,16 @@ class CommentView(APIView):
             return Response(comment.data, status=status.HTTP_201_CREATED)
         else:
             return Response(comment.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CommentUpdateView(APIView):
+   def put(self, request, pk):
+        request.data['author'] = request.user.id
+        comment = get_object_or_404(Comment, pk=pk)
+        if request.user != comment.author:
+            raise PermissionDenied('Unauthorized user')
+        updated_comment = UpdatedCommentSerializer(comment, data=request.data)
+        if updated_comment.is_valid():
+            updated_comment.save()
+            return Response(updated_comment.data)
+        else:
+            return Response(updated_comment.errors, status=status.HTTP_400_BAD_REQUEST)
